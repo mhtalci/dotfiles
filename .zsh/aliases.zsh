cde867348e290b79dd3fb83473f745bb0969703b
mr4lci <mehmet.alci@outlook.com>
2024-02-29 12:20:31 +0100

    Initial Commit

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,13 @@
+# Alias
+# ---
+#
+alias k="kubectl"
+alias h="helm"
+alias tf="terraform"
+alias a="ansible"
+alias ap="ansible-playbook"
+
+alias code="open -a 'Visual Studio Code'"
+
+alias ls="eza --icons --group-directories-first"
+alias ll="eza --icons --group-directories-first -l"
---
+++ .zshrc
@@ -0,0 +1,9 @@
+[[ -f ~/.zsh/aliases.zsh ]] && source ~/.zsh/aliases.zsh
+[[ -f ~/.zsh/functions.zsh ]] && source ~/.zsh/functions.zsh
+[[ -f ~/.zsh/starship.zsh ]] && source ~/.zsh/starship.zsh
+
+# Load Starship
+eval "$(starship init zsh)"
+
+# Load Direnv
+eval "$(direnv hook zsh)"
